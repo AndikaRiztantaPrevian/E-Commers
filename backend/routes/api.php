839b7ed0187920245e446fab9afc7a5133e6a703
piaan <VianRiztanta71@gmail.com>
2024-04-11 14:36:45 +0700

    Route Api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,3 +13,10 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('/login', 'login');
     Route::post('/register', 'register');
 });
+
+Route::controller(ProductController::class)->group(function () {
+    Route::get('/getProductData', 'getDataProduct');
+    Route::post('/product/store', 'store');
+    Route::put('/product/update', 'update');
+    Route::delete('/product/delete', 'delete');
+})->middleware('auth:sanctum');
